0d9c9f83bc069eea1105b2937f80b10847f68c41
Yrish-1 <pineyrish@gmail.com>
2026-02-10 09:05:56 +0800

    Add project files.

---
+++ CurrencyConverter/CurrencyConverter.cs
@@ -0,0 +1,17 @@
+﻿
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Enter amount in USD: ");
+        double usd = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Enter exchange rate from USD to EUR: ");
+        double rate = Convert.ToDouble(Console.ReadLine());
+
+        double eur = usd * rate;
+
+        Console.WriteLine("Amount in EUR: " + eur);
+        Console.ReadKey();
+    }
+}
---
+++ InterestCalculator/InterestCalculator.cs
@@ -0,0 +1,20 @@
+﻿
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Enter principal amount: ");
+        double principal = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Enter rate of interest: ");
+        double rate = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Enter time in years: ");
+        double time = Convert.ToDouble(Console.ReadLine());
+
+        double simpleInterest = (principal * rate * time) / 100;
+
+        Console.WriteLine("Simple Interest: " + simpleInterest);
+        Console.ReadKey();
+    }
+}
