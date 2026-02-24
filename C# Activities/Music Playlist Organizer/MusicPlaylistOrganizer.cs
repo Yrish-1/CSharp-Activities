b37e6c837caead39eb803a7fc256437ef94fd20c
Yrish-1 <pineyrish@gmail.com>
2026-02-24 09:51:08 +0800

    Folder set up

---
+++ C# Activities/Music Playlist Organizer/MusicPlaylistOrganizer.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+namespace SongClass
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            double totalDuration = 0;
+
+            Console.Write("Songs to add: ");
+            int arrayLength = int.Parse(Console.ReadLine());
+
+            Song[] songs = new Song[arrayLength];
+
+            for (int i = 0; i < arrayLength; i++)
+            {
+                Console.WriteLine($"\nSong #{i + 1}");
+                Console.Write("\nTitle: ");
+                String songTitle = Console.ReadLine();
+                Console.Write("Artist: ");
+                String songArtist = Console.ReadLine();
+                Console.Write("Duration (minutes): ");
+                double songDuration = double.Parse(Console.ReadLine());
+
+                totalDuration += songDuration;
+
+                songs[i] = new Song
+                {
+                    title = songTitle,
+                    artist = songArtist,
+                    duration = songDuration
+                };
+            }
+
+            double averageDuration = totalDuration / arrayLength;
+
+            Console.WriteLine("\n== || MY PLAYLIST || ==");
+            Console.WriteLine("Title\tArtist\tTime");
+            Console.WriteLine("--------------------------------------------");
+            for (int i = 0; i < arrayLength; i++)
+            {
+                songs[i].DisplaySong();
+            }
+
+            Console.WriteLine("\nTotal Duration: {0:F2} mins", totalDuration);
+            Console.WriteLine("Average Duration: {0:F2} mins", averageDuration);
+        }
+
+        public class Song
+        {
+            public String title;
+            public String artist;
+            public double duration;
+            public Song() : this("Unknown", "Unknown")
+            {
+            }
+
+            public Song(String title, String artist) : this(title, artist, 0)
+            {
+            }
+
+            public Song(String title, String artist, double duration)
+            {
+                this.title = title;
+                this.artist = artist;
+                this.duration = duration;
+            }
+
+            public void DisplaySong()
+            {
+                Console.WriteLine($"{title.PadRight(20)}{artist.PadRight(20)}{duration}");
+            }
+        }
+    }
+}
---
+++ C# Activities/Pentagon/Pentagon.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace PentagonClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pentagon p1 = new Pentagon();
+            Pentagon p2 = new Pentagon(5);
+
+            Console.WriteLine("Pentagon p1");
+            Console.WriteLine("-------------------");
+            p1.Display();
+
+            Console.WriteLine("Pentagon p2");
+            Console.WriteLine("-------------------");
+
+            p2.Display();
+        }
+
+        public class Pentagon
+        {
+            private double side;
+
+            public Pentagon()
+            {
+                side = 0;
+            }
+
+            public Pentagon(double side)
+            {
+                this.side = side;
+            }
+
+            public double GetPerimeter()
+            {
+                return 5 * side;
+            }
+
+            public double GetArea()
+            {
+                return (Math.Sqrt(5 * (5 + 2 * Math.Sqrt(5))) * Math.Pow(side, 2)) / 4;
+            }
+
+            public void Display()
+            {
+                Console.WriteLine("Side length: {0}", side);
+                Console.WriteLine("Perimeter: {0:F3}", GetPerimeter());
+                Console.WriteLine("Area : {0:F3}", GetArea());
+            }
+        }
+    }
+}
